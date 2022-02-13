18ab6ae796adf87163953e314dd6627ffaeff6de
kazuma.hirakawa <k.hirakazu.0611@gmail.com>
2022-02-13 18:32:29 +0900

    成分の正常判定を修正

--- src/model/IngredientsList.ts
+++ src/model/IngredientsList.ts
@@ -1,7 +1,7 @@
 interface Ingredient {
     value: number
     name: string
-    calc(): boolean
+    calc(): 'Upper' | 'Lower' | 'Standard'
 }
 /**
  * LDLコレステロール
@@ -10,11 +10,13 @@ export class LdlCholesterol implements Ingredient {
     value = 0
     name = 'LDLコレステロール'
 
-    calc(): boolean {
-        if (this.value <= 119 || this.value >= 70) {
-            return true
+    calc() {
+        if (this.value > 119) {
+            return 'Upper'
+        } else if (this.value < 70) {
+            return 'Lower'
         } else {
-            return false
+            return 'Standard'
         }
     }
 }
@@ -25,13 +27,14 @@ export class HdlCholesterol implements Ingredient {
     value = 0
     name = 'HDLコレステロール'
 
-    calc(): boolean {
-        if (this.value >= 70) {
-            return true
+    calc() {
+        if (this.value < 40) {
+            return 'Lower'
         } else {
-            return false
+            return 'Standard'
         }
     }
+
 }
 /**
  * 総コレステロール
@@ -40,11 +43,13 @@ export class TotalCholesterol implements Ingredient {
     value = 0
     name = '総コレステロール'
 
-    calc(): boolean {
-        if (this.value >= 130 || this.value <= 219) {
-            return true
+    calc() {
+        if (this.value > 219) {
+            return 'Upper'
+        } else if (this.value < 130) {
+            return 'Lower'
         } else {
-            return false
+            return 'Standard'
         }
     }
 }
@@ -55,11 +60,11 @@ export class MaximumBloodPressure implements Ingredient {
     value = 0
     name = '最高血圧'
 
-    calc(): boolean {
-        if (this.value <= 120) {
-            return true
+    calc() {
+        if (this.value > 120) {
+            return 'Upper'
         } else {
-            return false
+            return 'Standard'
         }
     }
 }
@@ -70,11 +75,11 @@ export class MinimumBloodPressure implements Ingredient {
     value = 0
     name = '最低血圧'
 
-    calc(): boolean {
-        if (this.value <= 80) {
-            return true
+    calc() {
+        if (this.value > 80) {
+            return 'Lower'
         } else {
-            return false
+            return 'Standard'
         }
     }
 }
